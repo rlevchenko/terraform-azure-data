4c0fbda029c27f3724779121d1a5561428e4c471
rlevchenko <c0rte0us@gmail.com>
2021-06-08 17:32:05 +0300

    fixed versions

--- auth.tf
+++ auth.tf
@@ -13,7 +13,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 1.35" # the latest is recommended/ tested with 2.62.0
+      version = ">= 2.62" # the latest is recommended/ tested with 2.62.0
     }
   }
 }
